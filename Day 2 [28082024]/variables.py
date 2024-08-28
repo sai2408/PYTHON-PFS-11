4006c05319a3cd32d763d4085f7ea3c2ab5f0070
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-08-28 17:49:45 +0530

    Add files via upload

---
+++ Day 2 [28082024]/variables.py
@@ -0,0 +1,84 @@
+#Variables
+#Naming Rules
+'''
+_10 = 100
+print(_10)
+'''
+#Assignment
+'''
+a = 10
+print(a)
+'''
+#Reassignment
+'''
+a = 10
+print(a)
+a = 100
+print(a)
+'''
+#Multiple assignment
+#Model 1
+'''
+a,b,c,d = 10,20,30,40
+print(a)
+print(b)
+print(c)
+print(d)
+'''
+#Model 2
+'''
+a = b = c = d = 100
+print(a)
+print(b)
+print(c)
+print(d)
+'''
+#Swapping
+'''
+a = 10
+b = 20
+print(a)
+print(b)
+a,b = b,a
+print(a)
+print(b)
+'''
+#Deleting Variables
+'''
+a = 10
+print(a)
+del a
+print(a)
+'''
+#Identifiers
+#Case 1
+'''
+a = 100
+b = 200
+print(a)
+print(id(a))
+print(b)
+print(id(b))
+'''
+#Case 2
+'''
+a = 100
+b = 100
+print(a)
+print(id(a))
+print(b)
+print(id(b))
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
